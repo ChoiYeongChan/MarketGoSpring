0c1a14eb1405c9bfc5720b095665ac94d1a04ee1
ChoiYeongChan <supaul97@naver.com>
2023-04-05 13:20:06 +0900

    일부 수정

--- src/main/java/com/example/marketgospring/MarketGoSpringApplication.java
+++ src/main/java/com/example/marketgospring/MarketGoSpringApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketGoSpringApplication {
-
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
     public static void main(String[] args) {
         SpringApplication.run(MarketGoSpringApplication.class, args);
     }
