36f0d9ba4ad88940ba8c980546084ce2f7a20efb
ChoiYeongChan <supaul97@naver.com>
2023-05-30 20:24:54 +0900

    230530 ner 오류 수정

--- src/main/java/com/example/marketgospring/entity/Json.java
+++ src/main/java/com/example/marketgospring/entity/Json.java
@@ -1,10 +1,32 @@
 package com.example.marketgospring.entity;
 
+import lombok.*;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "json")
 public class Json {
     @Id
     private Integer id;
+
+    @Column
+    private String imageName;
+
+    @Column
+    private String text1;
+
+    @Column
+    private String text2;
+
+    @Column
+    private String text3;
 }
